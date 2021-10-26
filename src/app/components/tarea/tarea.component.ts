69d8129a3eca8cd6b9e74355c504fb9b68de6d18
Ricardo Prieto <tersus.ra@gmail.com>
2021-10-26 11:42:37 -0500

    repositorio completo angular_lista

---
+++ src/app/components/tarea/tarea.component.ts
@@ -0,0 +1,41 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.css']
+})
+export class TareaComponent implements OnInit {
+
+
+  todo: string[] = [];
+  progreso: string[] = [];
+  final: string[] = [];
+
+  tarea: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  agregartarea(){
+    if(this.tarea === ''){
+      return;
+    }
+    this.todo.push(this.tarea);
+    this.tarea = '';
+  }
+}
